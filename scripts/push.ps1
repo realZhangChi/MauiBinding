47d3960dbdc7c5c7f2e3a12dd99b14fe75f8b6bf
zhangchi <zliming@outlook.com>
2022-09-28 13:33:22 +0800

    fix push script error

--- scripts/push.ps1
+++ scripts/push.ps1
@@ -2,11 +2,11 @@ $apiKey = $args[0]
 
 $rootPath = (Get-Item -Path "./" -Verbose).FullName
 
+Set-Location ./packages
+
 Get-ChildItem ./packages/*.nupkg -recurse | ForEach-Object -Process {
 	if ($_ -is [System.IO.FileInfo]) {
 		dotnet nuget push $_.Name -s https://api.nuget.org/v3/index.json --api-key "$apiKey"
-
-		Set-Location $rootPath
 	}
 }
 
