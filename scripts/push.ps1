491e5d7330e165eac3b9e4d97b53889c9944ffc2
zhangchi <zliming@outlook.com>
2022-09-28 13:42:58 +0800

    fix error

--- scripts/push.ps1
+++ scripts/push.ps1
@@ -4,7 +4,7 @@ $rootPath = (Get-Item -Path "./" -Verbose).FullName
 
 Set-Location ./packages
 
-Get-ChildItem ./packages/*.nupkg -recurse | ForEach-Object -Process {
+Get-ChildItem ./*.nupkg -recurse | ForEach-Object -Process {
 	if ($_ -is [System.IO.FileInfo]) {
 		dotnet nuget push $_.Name -s https://api.nuget.org/v3/index.json --api-key "$apiKey"
 	}
