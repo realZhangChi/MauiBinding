981e70083a53bd569a3c6a4c14cccfae7a80aa5d
ChiZhang <zliming@outlook.com>
2023-02-25 10:13:21 +0800

    try fix error

---
+++ src/AspectJ/MauiBinding.Android.AspectJ.AspectJRT/Additions/DeclareAnnotationImpl.cs
@@ -0,0 +1,10 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Org.Aspectj.Internal.Lang.Reflect;
+
+public partial class DeclareAnnotationImpl
+{
+    public Org.Aspectj.Lang.Reflect.IDeclareAnnotation.Kind? GetKind()
+    {
+        return Kind;
+    }
+}
