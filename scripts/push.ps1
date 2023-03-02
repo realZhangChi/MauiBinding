e1e05ea3f2604f059eb98ad651285af2298c9181
ChiZhang <zliming@outlook.com>
2023-03-02 12:55:27 +0800

    refactor

--- scripts/push.ps1
+++ scripts/push.ps1
@@ -2,9 +2,9 @@ $apiKey = $args[0]
 
 $rootPath = (Get-Item -Path "./" -Verbose).FullName
 
-Set-Location ./packages
+Set-Location $rootPath
 
-Get-ChildItem ./*.nupkg -recurse | ForEach-Object -Process {
+Get-ChildItem src/**/bin/Release/*.nupkg -recurse | ForEach-Object -Process {
 	if ($_ -is [System.IO.FileInfo]) {
 		dotnet nuget push $_.Name -s https://api.nuget.org/v3/index.json --api-key "$apiKey" --skip-duplicate
 	}
--- src/HuaweiCloudMeeting/MauiBinding.Android.HuaweiCloudMeeting.HWMCLink/Additions/AddContactItemAdapter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Com.Huawei.Contact.Adapter;
-
-public partial class AddContactItemAdapter
-{
-    public override Java.Lang.Object? GetItem(int position)
-    {
-        throw new NotImplementedException();
-    }
-}
