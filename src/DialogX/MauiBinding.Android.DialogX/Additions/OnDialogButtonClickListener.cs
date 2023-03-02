26e3ab884c5bae7425b17bb1a29e150466a08ad5
ChiZhang <zliming@outlook.com>
2023-03-02 10:29:54 +0800

    test

---
+++ src/DialogX/MauiBinding.Android.DialogX/Additions/OnDialogButtonClickListener.cs
@@ -0,0 +1,13 @@
+﻿using Android.Views;
+
+using Com.Kongzue.Dialogx.Interfaces;
+
+namespace MauiBinding.Android.DialogX.Additions;
+
+public class OnDialogButtonClickListener : Java.Lang.Object, IOnDialogButtonClickListener
+{
+    public bool OnClick(Java.Lang.Object? p0, View? p1)
+    {
+        throw new NotImplementedException();
+    }
+}
