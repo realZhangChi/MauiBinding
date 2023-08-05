4d2459ebc2c556f4370d8edb9a8e12966996700d
zhangchi <hi@zhangchi.io>
2023-08-05 20:13:18 +0800

    stripe

---
+++ src/Stripe/Android/Stripe.Payments.Core/Additions/StripePaymentAuthWebViewActivityBinding.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.Runtime;
+
+// ReSharper disable once CheckNamespace
+namespace Com.Stripe.Android.Databinding;
+
+public partial class StripePaymentAuthWebViewActivityBinding
+{
+    public unsafe global::Android.Views.View Root
+    {
+
+        get
+        {
+            const string __id = "getRoot.()Landroid/widget/RelativeLayout;";
+            try
+            {
+                var __rm = _members.InstanceMethods.InvokeAbstractObjectMethod(__id, this, null);
+                return global::Java.Lang.Object.GetObject<global::Android.Views.View>(__rm.Handle, JniHandleOwnership.TransferLocalRef)!;
+            }
+            finally
+            {
+            }
+        }
+    }
+}
+
---
+++ src/Stripe/Android/Stripe.Payments.Core/Additions/StripePaymentMethodsActivityBinding.cs
@@ -0,0 +1,24 @@
+﻿using Android.Runtime;
+
+// ReSharper disable once CheckNamespace
+namespace Com.Stripe.Android.Databinding;
+
+public partial class StripePaymentMethodsActivityBinding
+{
+    public unsafe global::Android.Views.View Root
+    {
+
+        get
+        {
+            const string __id = "getRoot.()Landroid/widget/RelativeLayout;";
+            try
+            {
+                var __rm = _members.InstanceMethods.InvokeAbstractObjectMethod(__id, this, null);
+                return global::Java.Lang.Object.GetObject<global::Android.Views.View>(__rm.Handle, JniHandleOwnership.TransferLocalRef)!;
+            }
+            finally
+            {
+            }
+        }
+    }
+}
